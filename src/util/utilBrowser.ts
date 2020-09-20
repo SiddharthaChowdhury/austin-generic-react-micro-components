7d7d819099053bddaf53a48c24b7301b9c25bbea
Siddhartha <siddharthac6@gmail.com>
2020-09-20 13:46:50 +0200

    Added styled components and custom btn color props added

--- src/util/utilBrowser.ts
+++ src/util/utilBrowser.ts
@@ -10,3 +10,16 @@ export function debounce (fn: any, delay: number) {
         }, delay)
     }
 }
+
+export function adjustColor (color: string, amount: number) {
+    /*
+        color: '#ColHex'
+        amount: -255 <- 0 -> 255
+            Where: 
+                value: 0    - is the exact color
+                value -255  - is the darkest of the color (ie black)
+                value 255   - is the lightest of the color (ie white)
+            Hint: play between -200 and 200
+    */
+    return '#' + color.replace(/^#/, '').replace(/../g, color => ('0'+Math.min(255, Math.max(0, parseInt(color, 16) + amount)).toString(16)).substr(-2));
+}
