e79943d6ec45e1429e1f843c46466af7e1fee444
Siddhartha Chowdhury <siddharthac6@gmail.com>
2020-10-26 23:02:50 +0100

    avatar src fall back added

--- src/avatar/Avatar.tsx
+++ src/avatar/Avatar.tsx
@@ -3,19 +3,36 @@ import React from "react";
 interface IAvatarProps {
     height?: number;
     src: string;
+    fallBackSrc?: string;
     [key: string]: any;
 }
 
-const Avatar: React.FC<IAvatarProps> = ({height, src, ...rest}) => {
+const Avatar: React.FC<IAvatarProps> = ({height, src, fallBackSrc, ...rest}) => {
+    const [imgOrigSrc, setSrc] = React.useState<string>();
+
     const square = `${height || 50}px`;
     const style: React.CSSProperties = {
-        backgroundImage: `url(${src})`,
+        backgroundImage: `url(${imgOrigSrc})`,
         backgroundRepeat: 'no-repeat',
         backgroundSize: "contain",
         height: square,
         width: square,
         borderRadius: '3px'
     };
+
+    React.useEffect(() => {
+        if(fallBackSrc) {
+            const img = new Image();
+            img.onload = function() {
+                setSrc(src);
+            }
+            img.onerror = function() {
+                setSrc(fallBackSrc);
+            }
+
+            img.src = src;
+        }
+    }, [])
 
     return (
         <div style={style} {...rest}/>
